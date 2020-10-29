e3f3b7ab18a81031a09383f7b64ef8fe19fd383c
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2020-10-29 17:44:34 -0300

    Desenvolvido, posicionado, estilizado e diagramado o rodape.

--- src/Components/footer/footer.jsx
+++ src/Components/footer/footer.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
-import { Container } from './styles-footer';
+import { Container, RowPais, RowMenu } from './styles-footer';
 
 const Footer = () => {
   return (
     <Container>
-      <h1>Footer</h1>
+      <RowPais>
+        Brasil
+      </RowPais>
+
+      <RowMenu>
+          <div className="Menu1">
+            <a href="/" className="MenuItem">Privacidade</a>
+            <a href="/" className="MenuItem">Termos</a>
+            <a href="/" className="MenuItem">Configurações</a>
+          </div>
+
+          <div className="Menu2">
+            <a href="/" className="MenuItem">Publicidade</a>
+            <a href="/" className="MenuItem">Negócios</a>
+            <a href="/" className="MenuItem">Sobre</a>
+            <a href="/" className="MenuItem">Como funciona a Pesquisa</a>
+          </div>
+      </RowMenu>
+
     </Container>
   );
 };
--- src/Components/footer/styles-footer.jsx
+++ src/Components/footer/styles-footer.jsx
@@ -8,3 +8,54 @@ export const Container = styled.div`
     }
     height:10vh;
 `;
+
+export const RowPais = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:flex-start;
+    align-items: center;
+
+    border-top:2px  solid #dadce0;
+    background-color: #f2f2f2;
+    
+    font-size:14px;
+    color:#717171;
+
+    padding: 10px 0 10px 20px;
+`;
+
+export const RowMenu = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:space-between;
+    align-items: center;
+    flex-wrap:wrap;
+
+    border-top:2px solid #dadce0;
+    border-bottom:2px solid #dadce0;
+    background-color:#f2f2f2;
+    
+
+    .MenuItem{
+        color: #717171;
+        text-decoration: none;
+        font-size:14px;
+
+        padding: 10px 15px;
+    }
+
+    @media(max-width:764px){
+        .Menu1:nth-child(1){
+            margin:0 auto;
+            padding: 10px 0;
+            background-color:#f2f2f2;
+        }
+
+        .Menu2:nth-child(2){
+            margin: 0 auto;
+            padding: 10px 0;
+            background-color:#f2f2f2;
+        }
+    }
+
+`;
